85fe8cac406bab4a144ade5757e8f0832da4764d
ynsaltn90 <ynsaltnkurs@gmail.com>
2022-12-29 00:13:24 +0300

    Sign In Matrix page1

--- app.js
+++ app.js
@@ -1,161 +1,182 @@
-(function() {
+(function () {
+  var doc = document,
+    elemList = Construct_SubElement_List(doc.body),
+    len = elemList.length,
+    win = window,
+    i,
+    The_Matrix;
 
-		var	doc = document ,
-		elemList = Construct_SubElement_List( doc.body ) ,
-		len = elemList.length ,
-		win = window ,
-		i ,
-		The_Matrix ;
+  for (i = 0; i < len; i++) {
+    var currElem = elemList[i];
 
-	for (  i = 0 ; i < len ; i++  ) {
+    if (
+      currElem.className &&
+      /^Block_The_Matrix_Viewport/.test(currElem.className)
+    ) {
+      The_Matrix = new The_Matrix_Obj(currElem);
+    }
+  }
+  function The_Matrix_Obj(ElemObjRef) {
+    var ELEM = ElemObjRef,
+      doc = document,
+      win = window,
+      span_EO = doc.createElement("span"),
+      div_EO = doc.createElement("div"),
+      docFrag = doc.createDocumentFragment(),
+      ElemList = Construct_SubElement_List(ELEM),
+      num = ElemList.length,
+      j,
+      k;
 
-		var	currElem	= elemList[i] ;
+    this.ElemObj = ELEM;
 
-		if (  currElem.className && /^Block_The_Matrix_Viewport/.test( currElem.className )  ) {
+    this.Cmd_Line_EO = "";
+    this.Code_EO = "";
+    this.Cursor_EO = "";
 
-			The_Matrix	=	new  The_Matrix_Obj( currElem ) ;
-		}
-	}
-	function	The_Matrix_Obj   (  ElemObjRef  )  {
+    this.Cmd_Typing = false;
 
-		var	ELEM		= ElemObjRef , doc = document , win = window , span_EO = doc.createElement("span") , div_EO = doc.createElement("div") , docFrag = doc.createDocumentFragment() ,
+    for (j = 0; j < num; j++) {
+      var CE = ElemList[j];
 
-			ElemList	= Construct_SubElement_List( ELEM ) , num = ElemList.length , j , k ;
+      if (/^Command_Line_sB/.test(CE.className)) this.Cmd_Line_EO = CE;
+      else if (/^Matrix_Code_sB/.test(CE.className)) this.Code_EO = CE;
+      else if (/^blinking_cursor/.test(CE.className)) this.Cursor_EO = CE;
+    }
 
-		this.ElemObj		= ELEM ;
+    this.Command_Line = function (cmdStr) {
+      if (!this.Cmd_Typing) {
+        this.Clear_HTML("cmd");
 
-		this.Cmd_Line_EO	= "" ;
-		this.Code_EO		= "" ;
-		this.Cursor_EO		= "" ;
+        this.Cmd_Line_EO.appendChild(
+          Construct_Element_Object(
+            span_EO.cloneNode(),
+            [["className", "blinking_cursor"]],
+            "",
+            "",
+            ""
+          )
+        );
+        this.Cmd_Line_EO.insertBefore(
+          span_EO.cloneNode(),
+          this.Cmd_Line_EO.firstChild
+        );
+        this.Cmd_Line_EO.firstChild.innerHTML = cmdStr.charAt(0);
+        this.Cmd_Typing = true;
+      } else {
+        this.Cmd_Line_EO.firstChild.innerHTML += cmdStr.charAt(0);
 
-		this.Cmd_Typing	= false ;
+        if (cmdStr.length == 1) {
+          this.Cmd_Typing = false;
+          return;
+        }
+      }
+      win.setTimeout(function () {
+        The_Matrix.Command_Line(cmdStr.substring(1));
+      }, 15);
+    };
 
-		for ( j = 0 ; j < num ; j++ ) {
+    this._Generate = function () {
+      this.Clear_HTML("matrix");
 
-			var	CE = ElemList[j] ;
+      var numCols = 90,
+        numRows = 80;
 
-			if ( /^Command_Line_sB/.test( CE.className ) )
+      for (j = 0; j < numCols; j++) {
+        var colSpan = span_EO.cloneNode();
 
-				this.Cmd_Line_EO	= CE ;
+        for (k = 0; k < numRows; k++) {
+          if (Math.random() <= 0.25)
+            var child_EO = Construct_Element_Object(
+              doc.createElement("b"),
+              "",
+              "",
+              [doc.createTextNode(Math.floor(Math.random() * 10))],
+              ""
+            );
+          else
+            var child_EO = doc.createTextNode(Math.floor(Math.random() * 10));
 
-			else if ( /^Matrix_Code_sB/.test( CE.className ) )
+          colSpan.appendChild(
+            Construct_Element_Object(div_EO.cloneNode(), "", "", [child_EO], "")
+          );
+        }
+        docFrag.appendChild(colSpan);
+      }
+      this.Code_EO.appendChild(docFrag);
 
-				this.Code_EO		= CE ;
+      win.setTimeout(function () {
+        The_Matrix._Generate();
+      }, 600);
+    };
 
-			else if ( /^blinking_cursor/.test( CE.className ) )
+    this.Clear_HTML = function (clearType) {
+      if (clearType == "matrix")
+        while (this.Code_EO.firstChild)
+          this.Code_EO.removeChild(this.Code_EO.firstChild);
+      else if (clearType == "cmd")
+        while (this.Cmd_Line_EO.firstChild)
+          this.Cmd_Line_EO.removeChild(this.Cmd_Line_EO.firstChild);
+    };
 
-				this.Cursor_EO	= CE ;
-		}
+    win.setTimeout(function () {
+      The_Matrix.Command_Line(
+        "Call trans opt: received. 2-19-98 13:24:18 REC:Log>"
+      );
+    }, 500);
+    win.setTimeout(function () {
+      The_Matrix.Command_Line("Trace program: running");
+    }, 2500);
+    win.setTimeout(function () {
+      The_Matrix.Clear_HTML("cmd");
+      The_Matrix._Generate();
+    }, 3500);
+    win.setTimeout(function () {
+      The_Matrix.ElemObj.className = "Block_The_Matrix_Viewport";
+    }, 3600);
+  }
+  function Construct_Element_Object(
+    elem,
+    propertyArr,
+    methodArr,
+    childArr,
+    innerHTML_CharStr
+  ) {
+    var Elem_Obj = elem,
+      Properties = propertyArr,
+      Methods = methodArr,
+      Children = childArr,
+      inner_html = innerHTML_CharStr,
+      len,
+      i;
 
-		this.Command_Line = function ( cmdStr ) {
+    len = Properties.length;
 
-			if ( !this.Cmd_Typing ) {
-			
-				this.Clear_HTML("cmd") ;
+    for (i = 0; i < len; i++) Elem_Obj[Properties[i][0]] = Properties[i][1];
 
-				this.Cmd_Line_EO.appendChild( Construct_Element_Object( span_EO.cloneNode() , [ [ "className" , "blinking_cursor" ] ] , "" , "" , "" ) ) ;
-				this.Cmd_Line_EO.insertBefore( span_EO.cloneNode() , this.Cmd_Line_EO.firstChild ) ;
-				this.Cmd_Line_EO.firstChild.innerHTML = cmdStr.charAt(0) ;
-				this.Cmd_Typing = true ;
+    len = Methods.length;
 
-			} else {
+    for (i = 0; i < len; i++) Elem_Obj[Methods[i][0]] = Methods[i][1];
 
-				this.Cmd_Line_EO.firstChild.innerHTML += cmdStr.charAt(0) ;
+    len = Children.length;
 
-				if ( cmdStr.length == 1 ){
+    for (i = 0; i < len; i++) Elem_Obj.appendChild(Children[i]);
 
-					this.Cmd_Typing = false ;
-					return ;
-				}
-			}
-			win.setTimeout( function() { The_Matrix.Command_Line( cmdStr.substring(1) ); } , 15 ) ;
-		} ;
+    len = inner_html.length;
 
-		this._Generate = function () {
+    if (len > 0) Elem_Obj.innerHTML = inner_html;
 
-			this.Clear_HTML("matrix") ;
+    return Elem_Obj;
+  }
+  function Construct_SubElement_List(ElemObjRef) {
+    var _ELEM = ElemObjRef,
+      tmpList = _ELEM.getElementsByTagName("*"),
+      tmpLen = tmpList.length,
+      k,
+      ELEM_LIST = [];
 
-			var	numCols = 90 , numRows = 80 ;
+    for (k = 0; k < tmpLen; k++) ELEM_LIST.push(tmpList[k]);
 
-			for ( j = 0 ; j < numCols ; j++ ) {
-
-				var	colSpan	= span_EO.cloneNode() ;
-
-				for ( k = 0 ; k < numRows ; k++ ) {
-
-					if ( Math.random() <= 0.25 )
-
-						var	child_EO = Construct_Element_Object( doc.createElement("b") , "" , "" , [ doc.createTextNode(Math.floor(Math.random()*10)) ] , "" ) ;
-					else
-						var	child_EO = doc.createTextNode( Math.floor(Math.random()*10) ) ;
-
-					colSpan.appendChild( Construct_Element_Object( div_EO.cloneNode(), "", "", [ child_EO ], "" ) ) ;
-				}
-				docFrag.appendChild( colSpan ) ;
-			}
-			this.Code_EO.appendChild( docFrag ) ;
-
-			win.setTimeout( function(){ The_Matrix._Generate() ; } , 600 ) ;
-		} ;
-
-		this.Clear_HTML = function ( clearType ) {
-
-			if ( clearType == "matrix" )
-
-				while ( this.Code_EO.firstChild )	this.Code_EO.removeChild( this.Code_EO.firstChild ) ;
-
-			else if ( clearType == "cmd" )
-
-				while ( this.Cmd_Line_EO.firstChild )	this.Cmd_Line_EO.removeChild( this.Cmd_Line_EO.firstChild ) ;
-		} ;
-
-		win.setTimeout( function(){ The_Matrix.Command_Line( "Call trans opt: received. 2-19-98 13:24:18 REC:Log>" ) ; } , 500 ) ;
-		win.setTimeout( function(){ The_Matrix.Command_Line( "Trace program: running" ) ; } , 2500 ) ;
-		win.setTimeout( function(){ The_Matrix.Clear_HTML("cmd") ; The_Matrix._Generate() ; } , 3500 ) ;
-		win.setTimeout( function(){ The_Matrix.ElemObj.className = "Block_The_Matrix_Viewport" ; } , 3600 ) ;
-	}
-	function	Construct_Element_Object (   elem  ,  propertyArr  ,  methodArr  ,  childArr  ,  innerHTML_CharStr  )  {
-
-		var	Elem_Obj	= elem ,
-
-			Properties	= propertyArr ,
-
-			Methods		= methodArr ,
-
-			Children	= childArr ,
-
-			inner_html	= innerHTML_CharStr ,
-
-			len , i ;
-
-			len = Properties.length ;
-
-			for (  i = 0 ; i < len ; i++  )		Elem_Obj[ Properties[i][0] ] = Properties[i][1] ;
-
-			len = Methods.length ;
-
-			for (  i = 0 ; i < len ; i++  )		Elem_Obj[ Methods[i][0] ] = Methods[i][1] ;
-
-			len = Children.length;
-
-			for (  i = 0 ; i < len ; i++  )		Elem_Obj.appendChild( Children[i] ) ;
-
-			len = inner_html.length ;
-
-			if (  len > 0  )			Elem_Obj.innerHTML = inner_html ;
-
-			return		Elem_Obj ;
-	}
-	function	Construct_SubElement_List ( ElemObjRef ) {
-
-		var	_ELEM		= ElemObjRef ,
-
-			tmpList		= _ELEM.getElementsByTagName("*") , tmpLen = tmpList.length , k ,
-
-   			ELEM_LIST 	= [] ;
-
-		for (  k = 0 ; k < tmpLen ; k++  )		ELEM_LIST.push( tmpList[k] ) ;
-
-		return		ELEM_LIST ;
-	}
-})() ;
+    return ELEM_LIST;
+  }
+})();
